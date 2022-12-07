88a7838995a7c9daf0c5614f53bbdd1d253e4ded
Filip <paliucfilip@yahoo.com>
2022-12-07 20:42:23 +0200

    Connected app with firebase and logIn with firebase Authentication

--- src/screens/LogInScreen/LogInScreen.js
+++ src/screens/LogInScreen/LogInScreen.js
@@ -3,19 +3,42 @@ import React, { useState } from 'react'
 import Logo from '../../../assets/bus_logo.png'
 import CustomInput from "./CustomInput";
 import CustomButton from "./CustomButton";
+import { auth } from "../../../firebase"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"
+
 
 const LogInScreen = () => {
 
   const [username, setUsername] = useState('');
-
   const [password, setPassword] = useState('');
-
-  const onSignInPressed = () => {
-    console.warn("Sign In");
+  
+  const handleRegister = () => {
+    createUserWithEmailAndPassword(auth, username, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      console.log("Registered")
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+    });
   }
 
-  const onRegisterPressed = () => {
-    console.warn("Create account");
+  const handleSignIn = () => {
+    signInWithEmailAndPassword(auth, username, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        console.log("Signed");
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
   }
 
   return (
@@ -26,12 +49,12 @@ const LogInScreen = () => {
         resizeMode="contain"
       />
       <Text style={styles.appTitle}>Eco Travel</Text>
-      <CustomInput placeholder="Username" value={username} setvalue={setUsername} />
-      <CustomInput placeholder="Password" value={password} setvalue={setPassword} secureTextEntry={true} />
-      <CustomButton text="Log In" onPress={onSignInPressed} />
+      <CustomInput placeholder="Username" value={username} setvalue={text => setUsername(text)} />
+      <CustomInput placeholder="Password" value={password} setvalue={text => setPassword(text)} secureTextEntry={true} />
+      <CustomButton text="Log In" onPress={handleSignIn} />
       <Text style={{ color: 'white', marginTop: 25, marginBottom: 15}}>__________________    OR    __________________</Text>
       <Text style={{ color: 'white', marginBottom: 15 }}>No account yet? Make one now.</Text>
-      <CustomButton text="Create Account" onPress={onRegisterPressed} type="Register" />
+      <CustomButton text="Create Account" onPress={handleRegister} type="Register" />
     </KeyboardAvoidingView>
   )
 }
