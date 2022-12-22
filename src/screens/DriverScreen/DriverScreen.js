ed534c65795eb5a9fa747a7e068d250de2ad4217
DanielPantea <37868032+DanielPantea@users.noreply.github.com>
2022-12-22 02:06:06 +0200

    DriverScreen: foreground and backgroiund location tracker

--- src/screens/DriverScreen/DriverScreen.js
+++ src/screens/DriverScreen/DriverScreen.js
@@ -3,13 +3,30 @@ import React, {useEffect, useState} from 'react'
 import VehicleChooser from "./VehicleChooser";
 import * as Location from 'expo-location';
 import * as TaskManager from 'expo-task-manager';
-import * as Permissions from 'expo-permissions';
 
-const LOCATION_TRACKING = 'location-tracking';
+const LOCATION_TASK_NAME = 'LOCATION_TASK_NAME';
+let foregroundSubscription = null
+
+TaskManager.defineTask(LOCATION_TASK_NAME, async ({ data, error }) => {
+    if (error) {
+        console.error(error)
+        return
+    }
+    if (data) {
+        console.log("Location in background")
+        // Extract location coordinates from data
+        const { locations } = data
+        const location = locations[0]
+        if (location) {
+            console.log("Location in background", location.coords)
+        }
+    }
+})
 
 const DriverScreen = () => {
 
     const [vehicle, setVehicle] = useState({type:null,line:null,size:null,isSet:false});
+    const [position, setPosition] = useState(null)
 
     // useEffect(() => {
     //     if(vehicle.isSet) {
@@ -18,7 +35,13 @@ const DriverScreen = () => {
     // });
 
     const startLocationTracking = async () => {
-        await Location.startLocationUpdatesAsync(LOCATION_TRACKING, {
+        await Location.startLocationUpdatesAsync(TASK_NAME, {
+            showsBackgroundLocationIndicator: true,
+            foregroundService: {
+                notificationTitle: "Location",
+                notificationBody: "Location tracking in background",
+                notificationColor: "#fff",
+            },
             accuracy: Location.Accuracy.Highest,
             timeInterval: 5000,
             distanceInterval: 0,
@@ -26,21 +49,100 @@ const DriverScreen = () => {
         const hasStarted = await Location.hasStartedLocationUpdatesAsync(
             LOCATION_TRACKING
         );
+
         console.log('tracking started?', hasStarted);
+    };
+    const stopLocationTracking = async () => {
+        await Location.stopLocationUpdatesAsync(TASK_NAME)
     };
 
     useEffect(() => {
-        const config = async () => {
-            let res = await Permissions.askAsync(Permissions.LOCATION_BACKGROUND);
-            if (res.status !== 'granted') {
-                console.log('Permission to access location was denied');
-            } else {
-                console.log('Permission to access location granted');
-            }
-        };
-
-        config();
+        const requestPermissions = async () => {
+            const foreground = await Location.requestForegroundPermissionsAsync()
+            if (foreground.granted) await Location.requestBackgroundPermissionsAsync()
+        }
+        requestPermissions()
     }, []);
+
+    // Start location tracking in foreground
+    const startForegroundUpdate = async () => {
+        // Check if foreground permission is granted
+        const { granted } = await Location.getForegroundPermissionsAsync()
+        if (!granted) {
+            console.log("location tracking denied")
+            return
+        }
+
+        // Make sure that foreground location tracking is not running
+        foregroundSubscription?.remove()
+
+        // Start watching position in real-time
+        foregroundSubscription = await Location.watchPositionAsync(
+            {
+                // For better logs, we set the accuracy to the most sensitive option
+                accuracy: Location.Accuracy.BestForNavigation,
+            },
+            location => {
+                console.log(location.coords)
+                setPosition(location.coords)
+            }
+        )
+    }
+
+    // Stop location tracking in foreground
+    const stopForegroundUpdate = () => {
+        foregroundSubscription?.remove()
+        setPosition(null)
+    }
+
+    // Start location tracking in background
+    const startBackgroundUpdate = async () => {
+        // Don't track position if permission is not granted
+        const { granted } = await Location.getBackgroundPermissionsAsync()
+        if (!granted) {
+            console.log("location tracking denied")
+            return
+        }
+
+        // Make sure the task is defined otherwise do not start tracking
+        const isTaskDefined = await TaskManager.isTaskDefined(LOCATION_TASK_NAME)
+        if (!isTaskDefined) {
+            console.log("Task is not defined")
+            return
+        }
+
+        // Don't track if it is already running in background
+        const hasStarted = await Location.hasStartedLocationUpdatesAsync(
+            LOCATION_TASK_NAME
+        )
+        if (hasStarted) {
+            console.log("Already started")
+            return
+        }
+
+        await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+            // For better logs, we set the accuracy to the most sensitive option
+            accuracy: Location.Accuracy.BestForNavigation,
+            // Make sure to enable this notification if you want to consistently track in the background
+            showsBackgroundLocationIndicator: true,
+            foregroundService: {
+                notificationTitle: "Location",
+                notificationBody: "Location tracking in background",
+                notificationColor: "#fff",
+            },
+        })
+    }
+
+    // Stop location tracking in background
+    const stopBackgroundUpdate = async () => {
+        const hasStarted = await Location.hasStartedLocationUpdatesAsync(
+            LOCATION_TASK_NAME
+        )
+        if (hasStarted) {
+            await Location.stopLocationUpdatesAsync(LOCATION_TASK_NAME)
+            console.log("Location tacking stopped")
+        }
+    }
 
     if(vehicle.isSet == false) {
         return (
@@ -55,7 +157,8 @@ const DriverScreen = () => {
         return (
             <View style={styles.container}>
                 <Text>Driver Screen</Text>
-                <Button title="Start tracking" onPress={startLocationTracking} />
+                <Button title="Start tracking" onPress={startForegroundUpdate} />
+                <Button title="Stop tracking" onPress={stopForegroundUpdate} />
             </View>
         )
     }
@@ -72,22 +175,8 @@ const styles = StyleSheet.create({
     },
 
     container: {
-        margin: 'auto'
+        alignItems: 'center',
+        flex: 1,
+        marginTop: 200
     },
 })
-
-TaskManager.defineTask(LOCATION_TRACKING, async ({ data, error }) => {
-    if (error) {
-        console.log('LOCATION_TRACKING task ERROR:', error);
-        return;
-    }
-    if (data) {
-        const { locations } = data;
-        let lat = locations[0].coords.latitude;
-        let long = locations[0].coords.longitude;
-
-        console.log(
-            `${new Date(Date.now()).toLocaleString()}: ${lat},${long}`
-        );
-    }
-});
