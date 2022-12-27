5714d67c06d65759268d7cbf0ea0665cfac98bcd
Filip <paliucfilip@yahoo.com>
2022-12-27 11:48:09 +0200

    added fit camera considering the selected origin and destination

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,16 +1,17 @@
 import { Dimensions, StyleSheet, Text, View } from 'react-native'
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps'
 import { useSelector } from 'react-redux'
 import { selectDestination, selectOrigin } from './navSlice'
 import MapViewDirection from "react-native-maps-directions"
 import { GOOGLE_API_KEY } from '@env'
-import * as Location from "expo-location";
 
 const Map = ({currentLocation}) => {
 
     const origin = useSelector(selectOrigin);
     const destination = useSelector(selectDestination);
+    const mapRef = useRef(null);
+
     const defaultLocation = {
         lat: 45.7538355,
         lng: 21.2257474
@@ -32,10 +33,20 @@ const Map = ({currentLocation}) => {
         setLat(destination?.location.lat);
         setLng(destination?.location.lng);
     }, [destination]);
+    
+    useEffect(() => {
+      if( !origin || !destination) return;
+
+      mapRef.current.fitToSuppliedMarkers(["destination","origin"], {
+        edgePadding: {top: 50, right: 50, left: 50, bottom: 50}
+      });
+    }, [origin,destination])
+    
 
     if(currentLocation != null) {
         return (
             <MapView
+                ref={mapRef}
                 style={styles.map}
                 provider={PROVIDER_GOOGLE}
                 region={
@@ -62,9 +73,25 @@ const Map = ({currentLocation}) => {
                             latitude: destination.location.lat,
                             longitude: destination.location.lng
                         }}
+                        title="Destination"
+                        description={destination.description}
+                        identifier="destination"
                     />
                 )}
-            </MapView>
+
+                {origin?.location && (
+                    <Marker 
+                        coordinate={{
+                            latitude: origin.location.lat,
+                            longitude: origin.location.lng
+                        }}
+                        title="Origin"
+                        description={origin.description}
+                        identifier="origin"
+                        opacity={1}
+                    />
+                )}
+            </MapView> 
         )
     }
     else {
