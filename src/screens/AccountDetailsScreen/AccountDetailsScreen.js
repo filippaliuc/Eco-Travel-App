52915886a6a433914a291816f887edec6e530a0a
Filip <paliucfilip@yahoo.com>
2023-01-11 19:06:10 +0200

    AccountDetails UI and functionality

--- src/screens/AccountDetailsScreen/AccountDetailsScreen.js
+++ src/screens/AccountDetailsScreen/AccountDetailsScreen.js
@@ -1,11 +1,16 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity , Image, BackHandler} from 'react-native'
+import React, { useEffect, useState } from 'react'
 import NavigationBar from '../../components/NavigationBar/NavigationBar';
+import Logo from '../../../assets/bus_logo.png'
 import { useNavigation } from '@react-navigation/native';
 import { signOut } from 'firebase/auth';
 import { auth } from '../../../firebase';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import CustomButton from './CustomButton';
 
 const AccountDetailsScreen = () => {
+
+  const [user, setUser] = useState(null)
 
   const navigation = useNavigation();
   const handleLogOut = () => {
@@ -16,22 +21,32 @@ const AccountDetailsScreen = () => {
     });
   }
 
+  useEffect(() => {
+    setUser(auth.currentUser?.email)
+  }, [])
+  
+  console.log(user)
+
   return (
     <View style={styles.container}>
-      <Text>AccountDetailsScreen</Text>
-      <TouchableOpacity 
-        style={{backgroundColor: "green"}}
-        onPress={() => handleLogOut()}  
-      >
-        <Text style={{fontSize: 30}}>LogOut</Text>
-      </TouchableOpacity>
-      <TouchableOpacity 
-        style={{backgroundColor: "yellow"}}
-        onPress={() => navigation.navigate("DriverScreen")}  
-      >
-        <Text style={{fontSize: 30}}>DriverScreen</Text>
-      </TouchableOpacity>
-      < NavigationBar></NavigationBar>
+
+    <SafeAreaView style={styles.topContainer}>
+        <Image
+          source={Logo}
+          style={styles.logo}
+          resizeMode="contain"
+        />
+        <View style={styles.buttonsContainer}>
+          <CustomButton text={`User: ${user}`} type={'list'}/>
+          <CustomButton text={'Tichete valide'} type={'list'} onPress={ ()=>  navigation.navigate("ActiveTicketScreen")}/>
+          <CustomButton text={'Abonamente'} type={'list'}/>
+          <CustomButton text={'Notificari'} type={'list'}/>
+          <View style={{justifyContent: 'flex-end', flex: 1}}>
+            <CustomButton text={'LogOut'} type={'logout'} onPress={() => handleLogOut()}/>
+          </View>
+        </View>
+    </SafeAreaView>
+        < NavigationBar></NavigationBar>
     </View>
   )
 }
@@ -41,7 +56,43 @@ export default AccountDetailsScreen
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: '6%', 
-    justifyContent: 'flex-end'
+    justifyContent: 'flex-end',
+    backgroundColor: '#279032'
   },
+
+  topContainer: {
+    flex: 1,
+    justifyContent:'flex-start'
+  },  
+
+  buttonsContainer: {
+    flex: 1,
+    justifyContent:'flex-start',
+    backgroundColor: '#279032',
+  },
+
+  logo: {
+    marginTop: 40,
+    marginBottom: 10,
+    width: '100%',
+    height: '15%',
+    justifyContent:'flex-start',
+    alignSelf:'center',
+  },
+  border: {
+    borderTopWidth: 0.5,
+    borderColor: 'white'
+  },
+  listButtons:{
+    backgroundColor: '#80D562',
+    paddingVertical: 5,
+  },
+
+  logoutButton: {
+    backgroundColor: '#D42222',
+    paddingVertical: 10,
+    borderBottomWidth: 0.5,
+    borderBottomColor: 'black'
+  },
+
 })
